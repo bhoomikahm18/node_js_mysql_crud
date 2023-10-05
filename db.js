1874d7363fbb7061f7f7e99323702576409c8053
Bhoomika <bhoomikahm18@gmail.com>
2023-10-05 11:03:13 +0530

    Building connection with mysql

---
+++ db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2/promise');
+
+const mysqlPool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root123',
+    database: 'employee_db'
+})
+
+module.exports = mysqlPool;
